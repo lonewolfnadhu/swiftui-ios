e310f13b94978277a8b8393a4dba9c5032d11de7
Nadir Shah <lonewolfnadhu@gmail.com>
2023-03-28 13:26:18 +0100

    Project Added

---
+++ Swift UI App/Swift UI App/Swift_UI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_AppApp.swift
+//  Swift UI App
+//
+//  Created by Nadir Shah on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
