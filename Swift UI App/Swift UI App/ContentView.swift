241f94894aae9461168519d935553c1532dacf92
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-01 11:47:38 +0100

    Phase 2

--- Swift UI App/Swift UI App/ContentView.swift
+++ Swift UI App/Swift UI App/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack{
-                Image(systemName: "book").resizable().frame(width: 50, height: 50).foregroundColor(.purple)
-                VStack(alignment: .leading) {
-                    Text("A Tale of Two Cities").font(.title)
-                    Text("Charles Dickens").foregroundColor(.gray)
-                }
+        VStack{
+            VStack {
+                BookView(title: "A Tale of Two Cities", author: "Charles Dickens")
+                BookView(title: "Exhalation", author: "Ted Chiang")
+                BookView(title: "Sum", author: "David Eagleman")
+            }
+            List {
+                BookView(title: "A Tale of Two Cities", author: "Charles Dickens")
+                BookView(title: "Exhalation", author: "Ted Chiang")
+                BookView(title: "Sum", author: "David Eagleman")
+            }
+            Form {
+                BookView(title: "A Tale of Two Cities", author: "Charles Dickens")
+                BookView(title: "Exhalation", author: "Ted Chiang")
+                BookView(title: "Sum", author: "David Eagleman")
             }
         }
     }
@@ -24,5 +32,20 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BookView: View {
+    var title: String
+    var author: String
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "book").resizable().frame(width: 50, height: 50).foregroundColor(.purple)
+            VStack(alignment: .leading) {
+                Text(title).font(.title)
+                Text(author).foregroundColor(.gray)
+            }
+        }
     }
 }
