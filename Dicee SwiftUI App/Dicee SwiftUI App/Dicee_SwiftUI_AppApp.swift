9fe01250a53af6c63ddbfc31c5dc6a92f7ac0de8
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-01 13:22:49 +0100

    Dicee SwiftUI App added

---
+++ Dicee SwiftUI App/Dicee SwiftUI App/Dicee_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_AppApp.swift
+//  Dicee SwiftUI App
+//
+//  Created by Nadir Shah on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
