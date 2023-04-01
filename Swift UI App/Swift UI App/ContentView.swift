bec88a5bae30cccabe745f452a19c3489eedcc0b
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-01 11:42:47 +0100

    Phase 1

--- Swift UI App/Swift UI App/ContentView.swift
+++ Swift UI App/Swift UI App/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(.purple).edgesIgnoringSafeArea(.all)
-            Circle()
-            Button(action: {}) {
-                Text("Button")
+        VStack {
+            HStack{
+                Image(systemName: "book").resizable().frame(width: 50, height: 50).foregroundColor(.purple)
+                VStack(alignment: .leading) {
+                    Text("A Tale of Two Cities").font(.title)
+                    Text("Charles Dickens").foregroundColor(.gray)
+                }
             }
         }
     }
