72061173534732362974ac8e7fd6908d7a0aac9d
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-01 12:16:03 +0100

    I Am Rich App completed

--- I Am Rich/I Am Rich/ContentView.swift
+++ I Am Rich/I Am Rich/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
         }
-        .padding()
     }
 }
 
