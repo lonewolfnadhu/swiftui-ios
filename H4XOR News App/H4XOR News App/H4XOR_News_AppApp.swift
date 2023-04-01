d32b11c17f3a87f5c34e6585d39cea19aa22c3c2
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-01 15:27:14 +0100

    H4XOR News App added

---
+++ H4XOR News App/H4XOR News App/H4XOR_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_News_AppApp.swift
+//  H4XOR News App
+//
+//  Created by Nadir Shah on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
