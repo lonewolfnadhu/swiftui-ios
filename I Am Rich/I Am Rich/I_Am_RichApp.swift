5b4671432705c8c07b4101a4258bf83f892d3957
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-01 12:00:44 +0100

    I Am Rich App added

---
+++ I Am Rich/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Nadir Shah on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
