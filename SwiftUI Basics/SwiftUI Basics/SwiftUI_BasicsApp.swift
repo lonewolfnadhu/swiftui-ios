3052bb3a181e1e80ab7d982ec7f8b5017ae9f5e9
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-03 19:54:15 +0100

    SwiftUI Basics project added

---
+++ SwiftUI Basics/SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Nadir Shah on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
