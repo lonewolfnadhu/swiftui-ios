ca663f8e3e79bd306f58b57151d7528e4d7bd0da
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-04 17:33:31 +0100

    Types of label formats in SwiftUI

--- SwiftUI Basics/SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/SwiftUI Basics/ContentView.swift
@@ -11,8 +11,30 @@ import SwiftUI
 // MARK: Basic view structure
 struct ContentView: View {
     var body: some View {
-        Text("Sign In")
-            .font(.title)
+        VStack {
+            Text("Sign In")
+                .font(.title)
+            // MARK: Text
+            Text("Welcome")
+
+            // MARK: Add some styles to the text
+            Text("Welcome! I am Nadhu and I am developer. Also, loves to code!")
+                .font(.title)
+                .foregroundColor(.green)
+                .lineLimit(2)
+            
+            // MARK: Date
+            Text(Date(), style: .date)
+            
+            // MARK: Time
+            Text(Date(), style: .time)
+            
+            // MARK: Lebel (Add icon next to text)
+            Label("SwiftUI in a Nutshell", systemImage: "water.waves")
+            
+            // MARK: Lebel with URL
+            Link("Learn more!", destination: URL(string: "https://www.apple.com")!)
+        }
     }
 }
 
