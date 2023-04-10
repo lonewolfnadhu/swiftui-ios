5dda06c0744659a112b5c729d3a89f73477309bf
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-10 20:30:51 +0100

    Text alligned in seperate Model

--- SwiftUI Basics/SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/SwiftUI Basics/ContentView.swift
@@ -11,29 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Sign In")
-                .font(.title)
-            
-            // MARK: Text
-            Text("Welcome")
-
-            // MARK: Add some styles to the text
-            Text("Welcome! I am Nadhu and I am developer. Also, loves to code!")
-                .font(.title)
-                .foregroundColor(.green)
-                .lineLimit(2)
-            
-            // MARK: Date
-            Text(Date(), style: .date)
-            
-            // MARK: Time
-            Text(Date(), style: .time)
-            
-            // MARK: Lebel (Add icon next to text)
-            Label("SwiftUI in a Nutshell", systemImage: "water.waves")
-            
-            // MARK: Lebel with URL
-            Link("Learn more!", destination: URL(string: "https://www.apple.com")!)
+            TextSwiftUI()
         }
     }
 }
@@ -42,5 +20,35 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TextSwiftUI: View {
+    var body: some View {
+        
+        // MARK: Text - Adding 
+        Text("Sign In")
+            .font(.title)
+        
+        // MARK: Text
+        Text("Welcome")
+
+        // MARK: Add some styles to the text
+        Text("Welcome! I am Nadhu and I am developer. Also, loves to code!")
+            .font(.title)
+            .foregroundColor(.green)
+            .lineLimit(2)
+        
+        // MARK: Date
+        Text(Date(), style: .date)
+        
+        // MARK: Time
+        Text(Date(), style: .time)
+        
+        // MARK: Lebel (Add icon next to text)
+        Label("SwiftUI in a Nutshell", systemImage: "water.waves")
+        
+        // MARK: Lebel with URL
+        Link("Learn more!", destination: URL(string: "https://www.apple.com")!)
     }
 }
