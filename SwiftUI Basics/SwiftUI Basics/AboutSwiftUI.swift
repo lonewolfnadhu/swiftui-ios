37330e189bfa66ffc0bc281c2df7486956472ca4
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-13 08:30:37 +0100

    SwiftUI Basics - AboutSwiftUI Updated

--- SwiftUI Basics/SwiftUI Basics/AboutSwiftUI.swift
+++ SwiftUI Basics/SwiftUI Basics/AboutSwiftUI.swift
@@ -12,22 +12,21 @@ struct AboutSwiftUI: View {
         VStack {
             Text("SwiftUI").font(.title)
             
+            Divider()
+            
             Text("SwiftUI is a modern declaration user interface framework for building iOS, iPadOS, macOS, watchOS, and tvOS apps using the Swift programming language.")
                 .lineLimit(4)
-                .padding(.horizontal)
             
             Divider()
             
             Text("It provides a single and intuitive way to create user interfaces with a live preview, enabling developers to see their changes in real-time.")
                 .lineLimit(4)
-                .padding(.horizontal)
             
             Divider()
             
             Text("SwiftUI also offers a range of build-in user interface controls and supports dynamic type, and dark mode out of the box.")
                 .lineLimit(4)
-                .padding(.horizontal)
-        }
+        }.padding(.all)
     }
 }
 
